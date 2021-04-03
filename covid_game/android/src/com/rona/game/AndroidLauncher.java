6265ae1d530ee31c29fde73be86548931b71a709
Sabine Salnave <sdsalnave@iCloud.com>
2021-04-03 18:46:11 -0500

    file w/ android folder- read only?

---
+++ covid_game/android/src/com/rona/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.rona.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.rona.game.Coronagamevirus;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Coronagamevirus(), config);
+	}
+}
